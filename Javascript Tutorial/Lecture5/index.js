5a4c41f8a521081c985a888eec52c0ad5ff4b7b0
rohitjha424 <rohitjha424@gmail.com>
2024-06-22 20:50:43 +0530

    Added Practoce Codes

---
+++ Javascript Tutorial/Lecture5/index.js
@@ -0,0 +1,28 @@
+// Question 1
+
+let a= "Rohit"
+a= a+10;
+
+// Question 2 
+
+console.log(typeof a) 
+
+//Question 3,4
+
+const c = {
+    "Harry":5,
+    "Rohit": 34 
+}
+c['Naima']=6
+console.log(c)
+//Question 5 ----- Dictionary
+
+const Dic = {
+    "Apple": "A Type of Fruit which keeps doctor Away",
+    "Pooke": "Gen Z Slang oftenly used by the Couples",
+    "Meloni": "Modi's weekness and Power",
+    "Inayat": "A good Name",
+    "Pen": "Used for writing on paper"
+}
+
+console.log(Dic.Apple)
---
+++ Javascript Tutorial/Lecture6/index.js
@@ -0,0 +1,83 @@
+// Arithematic Operators 
+console.log("Arithematic Operators");
+
+let a =45; console.log("a=",a);
+let b =4; console.log("b=",b);
+
+console.log("a+b=", a+b)
+
+console.log("a-b=", a-b)
+
+console.log("a*b=", a*b)
+
+console.log("a/b=", a/b)
+
+console.log("a%b=", a/b)     // gives reminder 
+
+console.log("a++ =", a++)           // increment
+
+console.log("a-- =", a--)           // decrement
+
+console.log("a=", a)   
+
+
+console.log("a**b=", a**b)      // a power b 
+
+
+
+// Assignment Operators
+
+console.log("Assignment Operators");
+
+let c = 10; console.log("c=",c);
+
+c += 5;  // same as a= a+5
+console.log("c +=5 is " , c)
+
+c -= 5;
+console.log("c -= 5  is ", c)
+
+c *= 5;
+console.log("c *= 5 is ", c)
+
+c /= 5;
+console.log("c /= 5 is ", c)
+
+
+
+// Comparison Operators
+console.log("Comparison Operators");
+
+let comp1 = 10;  console.log("comp1=",comp1);
+let comp2 = 20;  console.log("comp2=",comp2);
+let comp3 = "10"; console.log("comp3=",comp3);
+
+console.log("comp1 == comp2 is ", comp1 == comp2)
+
+console.log("comp1 != comp2 is ", comp1 != comp2)
+
+console.log("comp1 == comp3 is ", comp1 == comp3, )  // only value is compared in ==
+
+console.log("comp1 === comp3 is ", comp1 === comp3, 
+    "as typeof comp2 is ", typeof(comp2), "but typeof comp3 is ",  typeof(comp3))  //both value and type is compared 
+
+
+console.log("comp1 !== comp3 is ", comp1 !== comp3)  // !== checked if Value or type is not equal
+
+
+
+//Logical operators
+console.log("Logical Operators");
+
+
+let x=10; console.log("x=",x);
+let y = 11;  console.log("y=",y);
+
+console.log("x > y = ", x > y)
+
+console.log("x < y && x=10 is ",x < y && x==10 )  // Logical AND 
+
+console.log("x < y ||  x=11 is ",x < y && x==10 )  //Logical OR 
+
+console.log("!true", !true)  // Logical NOT
+console.log("!false", !false)   // Logical NOT
